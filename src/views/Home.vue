c7bf754f05bfa8264e8c18ab0d87fc63b0efe894
Chris Karanikas <ckaranikas94@gmail.com>
2021-06-14 23:42:18 +0300

    change

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App abou calisthenix!" />
+    <HelloWorld msg="Welcome to Your Vue.js App abou calisthenix!1" />
   </div>
 </template>
 
