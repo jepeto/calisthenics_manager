8e483b7ff4117342cc88fd3fc3629385278827f4
Christoforos Vradis <chrisvradis00@gmail.com>
2021-06-15 22:51:49 +0300

    Change navbar css, Add nutrition and Exercises Tab

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,20 +2,22 @@
 
 <template>
   <header>
-    <div>
-      <nav>
-        <ul>
-          <li><router-link to="/">Home</router-link></li>
-          <li><router-link to="/dashboard">Dashboard</router-link></li>
-          <li><router-link to="/equipment">Equipment</router-link></li>
-          <li><router-link to="/parks">Parks</router-link></li>
-          <li><router-link to="/contact">Contact</router-link></li>
-          <li><router-link to="/login">Login</router-link></li>
-          <li><router-link to="/signup">Sign up</router-link></li>
-          <li><router-link to="/logout">Logout</router-link></li>
-        </ul>
-      </nav>
-    </div>
+    <nav class="navbar flex h-20 border-b">
+      <div class="flex flex-auto">
+        <router-link to="/">Home</router-link>
+        <router-link to="/dashboard">Dashboard</router-link>
+        <router-link to="/equipment">Equipment</router-link>
+        <router-link to="/nutrition">Nutrition</router-link>
+        <router-link to="/exercises">Exercises</router-link>
+        <router-link to="/parks">Parks</router-link>
+        <router-link to="/contact">Contact</router-link>
+      </div>
+      <div class="flex flex-grow justify-end">
+        <router-link to="/login">Login</router-link>
+        <router-link to="/signup">Sign up</router-link>
+        <router-link to="/logout">Logout</router-link>
+      </div>
+    </nav>
   </header>
 </template>
 
@@ -24,3 +26,17 @@ export default {
   name: "NavBar",
 };
 </script>
+
+<style scoped>
+.navbar a {
+  display: flex;
+  margin-left: 1rem;
+  margin-right: 1rem;
+  justify-content: center;
+  align-items: center;
+}
+
+.navbar a:hover {
+  border-bottom: 2px solid #1a7676;
+}
+</style>
---
+++ src/views/Nutrition.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="nutrition">
+    <h2 class="text-3xl">Nutrition</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Nutrition",
+};
+</script>
+
+<style></style>
