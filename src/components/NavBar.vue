06c8420ee3207a6629266b7fbede4b89c10d95d3
Christoforos Vradis <chrisvradis00@gmail.com>
2021-06-17 13:25:08 +0300

    Make hamburger responsive - Not working if an option is slected from menu or if user clicks outside menu

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -186,21 +186,13 @@
 
         <!-- Mobile Button Goes Here -->
         <div class="lg:hidden flex items-center">
-          <button class="mobile-menu-toggler">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              class="h-6 w-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
+          <button
+            class="mobile-menu-toggler hamburger hamburger--collapse"
+            type="button"
+          >
+            <span class="hamburger-box">
+              <span class="hamburger-inner"></span>
+            </span>
           </button>
         </div>
       </div>
@@ -374,10 +366,20 @@ export default {
     const menuToggler = document.querySelector("button.mobile-menu-toggler");
 
     menuToggler.addEventListener("click", () => {
+      !this.$store.getters.getMenuState
+        ? menuToggler.classList.add("is-active")
+        : menuToggler.classList.remove("is-active");
+
       this.$store.dispatch("toggleMenu", !this.$store.getters.getMenuState);
     });
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.hamburger:focus,
+.hamburger:active {
+  outline: none;
+  box-shadow: none;
+}
+</style>
