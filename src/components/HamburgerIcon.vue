e140d276bc1bdef8eae8e14f35c6ae3a9d07f1c4
Christoforos Vradis <chrisvradis00@gmail.com>
2021-06-17 13:36:42 +0300

    Abstract away from navbar the hamburger icon and the mobile menu

---
+++ src/components/HamburgerIcon.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="lg:hidden flex items-center">
+    <button
+      class="mobile-menu-toggler hamburger hamburger--collapse"
+      type="button"
+    >
+      <span class="hamburger-box">
+        <span class="hamburger-inner"></span>
+      </span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const menuToggler = document.querySelector("button.mobile-menu-toggler");
+
+    //When hamburger icon is clicked
+    menuToggler.addEventListener("click", () => {
+      !this.$store.getters.getMenuState
+        ? menuToggler.classList.add("is-active")
+        : menuToggler.classList.remove("is-active");
+
+      this.$store.dispatch("toggleMenu", !this.$store.getters.getMenuState);
+    });
+  },
+};
+</script>
+
+<style scoped>
+.hamburger:focus,
+.hamburger:active {
+  outline: none;
+  box-shadow: none;
+}
+</style>
